03f17dc3987911e56eca3a0326a079d724d01ef5
Krishna Lokre <54632175+krishnaL-hub@users.noreply.github.com>
2019-09-11 12:21:51 +0530

    Create prime1.py

---
+++ Python program to print all Prime numbers in an Interval/prime1.py
@@ -0,0 +1,14 @@
+start=10
+end=20
+c=0 
+for val in range(start, end + 1): 
+    
+    if val > 1:
+       for n in range(2,val):
+           if (val % n) == 0: 
+            break
+       else: 
+            print(val) 
+            
+            c+=1 
+print("total number of prime number in range:",c)
