7f03eeb42a5cd417e5477770d7f1e63c2593ecac
krishnaL-hub <54632175+krishnaL-hub@users.noreply.github.com>
2019-09-11 10:17:43 +0530

    find Armstrong number using Function

---
+++ Python Program to check Armstrong Number/isarmstrong.py
@@ -0,0 +1,17 @@
+def isarmstrong(value):
+    l=len(value)
+    temp=int(value)
+    n=temp
+    sum1=0
+    while temp>0:
+        r=temp%10
+        sum1+=pow(r,l)
+        temp//=10
+    return sum1==n
+
+x=input("Enter the number : ")
+
+if isarmstrong(x):
+    print(x,"is a armstrong number")
+else:
+    print(x,"is not a armstrong number")
