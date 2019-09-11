4a8cefe11ab6d1447c76f377101e84eebc79e34a
Krishna Lokre <54632175+KrishnaL1996@users.noreply.github.com>
2019-09-11 19:18:07 +0530

    Update squaresum2.py
    int is addded

--- python program for sum of square of first n natural numbers/squaresum2.py
+++ python program for sum of square of first n natural numbers/squaresum2.py
@@ -3,3 +3,10 @@ def squaresum1(n):
     return (n*(n+1)*(2*n+1))/6
 n=500000 # max length support upto 5 digit
 print(squaresum1(n)) 
+
+
+#method 3
+def squaresum3(n):
+   return int((n*(n+1)*(2*n+1))/6)
+n=50000000000 
+print(squaresum3(n))
