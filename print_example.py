30e180624150e34c146f8bd5846cf5fd731b8f0f
Krishna Lokre <54632175+KrishnaL1996@users.noreply.github.com>
2025-10-18 17:47:23 +0530

    Enhance print statements with custom formatting
    Added print statements with custom separator and end parameters.

--- print_example.py
+++ print_example.py
@@ -2,3 +2,8 @@
 a = 5
 print("a =", a, sep='00000', end='\n\n\n')
 print("a =", a, sep='0', end='')
+
+
+#TODO
+#print(''' Statement "No, no, 'e's, u ...... "''')
+#print("""Statement "No, no, 'e's, u ...... " """)
